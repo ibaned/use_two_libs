4edd8199013948296e7956a176179ffa1dfb0f4b
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-12 13:53:50 -0400

    fix compilation issues...

--- use_two_libs.c
+++ use_two_libs.c
@@ -1,6 +1,7 @@
-#include <mult_nums.h>
-
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <mult_nums.h>
 
 int main(int argc, char** argv) {
   printf("%d\n", mult_nums(atoi(argv[1]), atoi(argv[2])));
