d37c3e5df9df7a900a4fa5eeaa5bf7f11e2a1bec
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-14 16:48:50 -0400

    use the bundled interface lib

--- use_two_libs.c
+++ use_two_libs.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 
 #include <mult_nums.h>
+#include <better_atoi.h>
 
 int main(int argc, char** argv) {
-  printf("%d\n", mult_nums(atoi(argv[1]), atoi(argv[2])));
+  printf("%d\n", mult_nums(
+        better_atoi(argv[1]),
+        better_atoi(argv[2])));
   return 0;
 }
